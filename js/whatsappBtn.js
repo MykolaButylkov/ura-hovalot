e2c20d5f6092009d3b844ca5ebe6c525d6871897
Mykola Butylkov <qakokos@gmail.com>
2026-01-28 19:33:00 +0200

    pervaya stranica gotova

---
+++ js/whatsappBtn.js
@@ -0,0 +1,5 @@
+  const phone = "9725280799448"; // твой номер
+  const text = "הי הגעתי מאתר אלכסו הובלות ואני מעוניין בהצעת מחיר עבור הובלה";
+
+  const url = `https://wa.me/${phone}?text=${encodeURIComponent(text)}`;
+  document.getElementById('waBtn').href = url;
